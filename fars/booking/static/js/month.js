9a8c47f1a08e27c19acb18ed3c182ae3fac19cee
CastorK <castor.kohler@aalto.fi>
2018-05-14 14:31:37 +0300

    Add left/right arrow for prev/next month

--- fars/booking/static/js/month.js
+++ fars/booking/static/js/month.js
@@ -47,4 +47,36 @@ $(document).ready(function() {
         });
       }
   });
+  var Key = {
+    LEFT:   37,
+    RIGHT:  39
+  };
+
+  /**
+   * old IE: attachEvent
+   * Firefox, Chrome, or modern browsers: addEventListener
+   */
+  function _addEventListener(evt, element, fn) {
+    if (window.addEventListener) {
+      element.addEventListener(evt, fn, false);
+    }
+    else {
+      element.attachEvent('on'+evt, fn);
+    }
+  }
+
+  function handleKeyboardEvent(evt) {
+    if (!evt) {evt = window.event;} // for old IE compatible
+    var keycode = evt.keyCode || evt.which; // also for cross-browser compatible
+    switch (keycode) {
+      case Key.LEFT:
+        calendar.fullCalendar('prev');
+        break;
+      case Key.RIGHT:
+        calendar.fullCalendar('next');
+        break;
+    }
+  }
+
+  _addEventListener('keydown', document, handleKeyboardEvent);
 });
