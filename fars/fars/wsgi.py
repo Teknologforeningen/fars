11ec5f3564341e1581c147f34d34cfced5467822
Thomas Langenskiöld <tlangens@gmail.com>
2018-09-12 20:44:59 +0300

    Read env file at entry points

--- fars/fars/wsgi.py
+++ fars/fars/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fars.settings")
 
 application = get_wsgi_application()
