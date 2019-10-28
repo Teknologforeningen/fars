7f0c6f0c96fbc44cba5d15d0b80e401a0438300a
CastorK <castor.kohler@aalto.fi>
2019-10-28 16:36:01 +0200

    Fix changes related to PR
    Moved css into separate file, did some stylechanges and codestyle"

--- fars/booking/tests.py
+++ fars/booking/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from booking.models import *
 # Create your tests here.
+class BookableTestCase(TestCase):
+    def setUp(self):
+        Bookable.objects.create(id_str="bookable1", name="Bookable1", description="A bookable")
+
+    def test_string_representation(self):
+        """Testing that string representation works correctly"""
+        bookable = Bookable.objects.get(id_str="bookable1")
+        self.assertEqual(str(bookable), "Bookable1")
