c179880c59be1a4b98593ad876271fe4c36ca283
CastorK <castor.kohler@aalto.fi>
2019-10-29 16:05:00 +0200

    Add tests for disallowed ways of booking

--- fars/booking/tests.py
+++ fars/booking/tests.py
@@ -17,6 +17,7 @@ class BookTestCase(TestCase):
             password = "pw"
         )
 
+
     def test_user_can_book(self):
         """A valid user can book a bookable"""
         c = Client()
@@ -31,3 +32,46 @@ class BookTestCase(TestCase):
         response = c.post('/booking/book/b', postdata)
 
         self.assertEqual(response.status_code, 201)
+
+
+    def test_cant_book_unauthorized(self):
+        """If not logged in, bookable cant be booked"""
+        c = Client()
+        now = datetime.now()
+        postdata = {
+            'before': now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'after': (now + timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'bookable': 'b'
+        }
+        response = c.post('/booking/book/b', postdata)
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_cant_book_in_the_past(self):
+        """Making a booking in the past shouldn't work"""
+        c = Client()
+        now = datetime.now()
+        postdata = {
+            'before': (now - timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'after': now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'bookable': 'b'
+        }
+
+        c.login(username='u', password='pw')
+        response = c.post('/booking/book/b', postdata)
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_cant_book_too_far_in_future(self):
+        """Making a booking too far in the future shouldn't be allowed"""
+        c = Client()
+        now = datetime.now()
+        postdata = {
+            'before': (now + timedelta(years=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'after': (now + timedelta(years=1, hours=1)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'bookable': 'b'
+        }
+
+        c.login(username='u', password='pw')
+        response = c.post('/booking/book/b', postdata)
+        self.assertEqual(response.status_code, 403)
