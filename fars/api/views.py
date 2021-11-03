e55f994772b754821fe5f8280c347dc706976c1c
Filip Stenbacka <filip.stenbacka@gmail.com>
2021-11-03 23:05:08 +0200

    Revert "Added bookings/past, bookings/future, bookings/now, bookings/next and bookings/previous API endpoints, which can all be filtered by bookable by providing a query parameter."
    This reverts commit acfdbeae3bc1c61206d69a82cc3f8994b2be33a3.

--- fars/api/views.py
+++ fars/api/views.py
@@ -1,7 +1,5 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.decorators import action
 from django_filters import rest_framework as filters
 from booking.models import *
 from api.serializers import *
@@ -27,65 +25,6 @@ class BookingsList(viewsets.ViewSetMixin, generics.ListAPIView):
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = BookingFilter
 
-    def filter_on_bookable(self, bookings, request):
-        bookable = request.query_params.get('bookable')
-        if bookable:
-            return bookings.filter(bookable__id_str=bookable), True
-        return bookings, False
-
-    def serialize_one(self, booking):
-        # If the booking is None, the serializer will create a null object, which we don't want
-        return Response(self.get_serializer_class()(booking).data if booking else None)
-
-    def serialize_many(self, bookings):
-        # If the booking is None, the serializer will create a null object, which we don't want
-        return Response(self.get_serializer_class()(bookings, many=True).data)
-
-    def filter_on_bookable_and_serialize_many(self, bookings, request):
-        bookings, _ = self.filter_on_bookable(bookings, request)
-        return self.serialize_many(bookings)
-
-    @action(methods=['get'], detail=False)
-    def future(self, request):
-        bookings = self.get_queryset().filter(start__gte=datetime.datetime.now())
-        return self.filter_on_bookable_and_serialize_many(bookings, request)
-
-    @action(methods=['get'], detail=False)
-    def past(self, request):
-        bookings = self.get_queryset().filter(end__lte=datetime.datetime.now())
-        return self.filter_on_bookable_and_serialize_many(bookings, request)
-
-    @action(methods=['get'], detail=False)
-    def next(self, request):
-        bookings = self.get_queryset().filter(start__gte=datetime.datetime.now())
-        bookings, _ = self.filter_on_bookable(bookings, request)
-
-        # Find the next booking
-        b = bookings.order_by('start').first()
-
-        return self.serialize_one(b)
-
-    @action(methods=['get'], detail=False)
-    def previous(self, request):
-        bookings = self.get_queryset().filter(end__lte=datetime.datetime.now())
-        bookings, _ = self.filter_on_bookable(bookings, request)
-
-        # Find the next booking
-        b = bookings.order_by('end').last()
-
-        return self.serialize_one(b)
-
-    @action(methods=['get'], detail=False)
-    def now(self, request):
-        t = datetime.datetime.now()
-        bookings = self.get_queryset().filter(start__lte=t, end__gte=t)
-        bookings, filtered = self.filter_on_bookable(bookings, request)
-
-        # Filtering on bookable should result in a maximum of one booking
-        if filtered:
-            return self.serialize_one(bookings.first())
-
-        return self.serialize_many(bookings)
 
 # This class provides the view used by GeneriKey to get the list of bookings they need
 class GeneriKeyBookingsList(viewsets.ViewSetMixin, generics.ListAPIView):
