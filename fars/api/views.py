07f658e091e94fa92c780c02e093d1b261499131
Filip Stenbacka <filip.stenbacka@gmail.com>
2021-11-09 00:34:14 +0200

    Revert "Implemented 'limit' query parameter for bookings API, which limits the amount of responses for a query."
    This reverts commit 11359aca39e3be4a077510585ee7e055eafedc89.

--- fars/api/views.py
+++ fars/api/views.py
@@ -29,12 +29,6 @@ class BookingsList(viewsets.ViewSetMixin, generics.ListAPIView):
     ordering_fields = ['start', 'end', 'id']
     ordering = ['start', 'end']
 
-    def finalize_response(self, request, response, *args, **kwargs):
-        limit = request.query_params.get("limit")
-        if limit and limit.isdigit():
-            response.data = response.data[:int(limit)]
-        return super().finalize_response(request, response, *args, **kwargs)
-
 
 # This class provides the view used by GeneriKey to get the list of bookings they need
 class GeneriKeyBookingsList(viewsets.ViewSetMixin, generics.ListAPIView):
